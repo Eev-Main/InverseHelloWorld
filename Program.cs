744b76305396e1db175470f89f505f7233dc067d
victor Gonzalez <victorantonio6@gmail.com>
2021-02-11 14:06:57 -0700

    subida de archivos

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InverseHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inverse = new string[2];
+            inverse[0] = "!dlroW";
+            inverse[1] = " olleH";
+
+            Console.WriteLine(inverse[0]+inverse[1]);
+        }
+    }
+}
